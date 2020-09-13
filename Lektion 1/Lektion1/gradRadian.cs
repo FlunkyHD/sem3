6ee25c31dbc5d76add3227e9e887b753aff3ddd7
Morten Jørgensen <mortenj2@hotmail.dk>
2020-09-13 18:29:08 +0200

    Start på opgaver til 1. lektion

---
+++ Lektion 1/Lektion1/gradRadian.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lektion1
+{
+    class gradRadian
+    {
+        public double Grad (double radian)
+        {
+            if (radian > 0 && radian < 2 * Math.PI)
+            {
+                return radian * (180 / Math.PI);
+            } else
+            {
+                throw new ArgumentException();
+            }
+            
+        }
+
+        public double Radian(double grad)
+        {
+            if (grad > 0 && grad < 360)
+            {
+                return grad * (Math.PI / 180);
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+
+        }
+    }
+}
